bc055b70f1351c6bfff6495d94ab996ab66a961a
vvusu <vvusu@vvusus-iMac.local>
2020-08-23 12:50:59 +0800

    update caddy

--- src/download-caddy.sh
+++ src/download-caddy.sh
@@ -3,9 +3,9 @@ _download_caddy_file() {
 	caddy_tmp_file="/tmp/install_caddy/caddy.tar.gz"
 	[[ -d $caddy_tmp ]] && rm -rf $caddy_tmp
 	if [[ $sys_bit == "i386" || $sys_bit == "i686" ]]; then
-		local caddy_download_link="https://caddyserver.com/download/linux/386?license=personal"
+		local caddy_download_link="https://caddyserver.com/api/download?os=linux&arch=386&license=personal"
 	else
-		local caddy_download_link="https://caddyserver.com/download/linux/amd64?license=personal"
+		local caddy_download_link="https://caddyserver.com/api/download?os=linux&arch=amd64&license=personal"
 	fi
 
 	mkdir -p $caddy_tmp
